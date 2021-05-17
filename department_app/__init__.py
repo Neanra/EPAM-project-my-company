9180797a63d4f256261526a0f85da0abf2f45b99
Julia Issajeva <julia.isajeva@gmail.com>
2021-05-17 08:13:57 +0300

    Created delete_department and base temlates. Extended deparments, employees and view_department templates.

--- department_app/__init__.py
+++ department_app/__init__.py
@@ -1,5 +1,5 @@
 from decimal import Decimal
-from flask import Flask, render_template
+from flask import Flask, render_template, request, abort, redirect, flash
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 
@@ -12,6 +12,7 @@ def create_app():
     app = Flask(__name__)
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://department_app:sleep42@localhost/department_app"
+    app.config["SECRET_KEY"] = "g5g5h4jytj65j56j"
     bootstrap.init_app(app)
     db.init_app(app)
 
@@ -27,6 +28,27 @@ def create_app():
         if not department:
             return "Department not found", 404
         return render_template("view_department.html.jinja", department=department)
+
+    @app.route("/departments/<int:id>/delete", methods=["GET","POST"])
+    def delete_department(id):
+        department = Department.query.get(id)
+        if not department:
+            return "Department not found", 404
+        if request.method == "POST":
+            try:
+                for employee in department.employees:
+                    db.session.delete(employee)
+                db.session.delete(department)
+                db.session.commit()
+                flash("Department {} deleted successfully!".format(department.name))
+            except:
+                flash("Department not deleted!")
+            finally:
+                return redirect("/departments")
+
+        else:
+            assert request.method == "GET"
+            return render_template("delete_department.html.jinja", department=department)
 
     @app.route("/employees")
     def employees():
