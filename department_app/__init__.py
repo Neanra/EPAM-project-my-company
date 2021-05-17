a04f4a51b5c505a0906b143f1a4bbd9ad283aeda
Julia Issajeva <julia.isajeva@gmail.com>
2021-05-17 08:50:27 +0300

    Added view_employee and delete_employee templates

--- department_app/__init__.py
+++ department_app/__init__.py
@@ -41,11 +41,13 @@ def create_app():
                 db.session.delete(department)
                 db.session.commit()
                 flash("Department {} deleted successfully!".format(department.name))
+                return redirect("/departments")
             except:
                 flash("Department not deleted!")
+                return redirect("/departments/{}".format(department.id))
+
             finally:
                 return redirect("/departments")
-
         else:
             assert request.method == "GET"
             return render_template("delete_department.html.jinja", department=department)
@@ -55,5 +57,30 @@ def create_app():
         employees = Employee.query.all()
         employees.sort(key=lambda x: x.full_name)
         return render_template("employees.html.jinja", employees=employees)
+
+    @app.route("/employees/<int:id>")
+    def view_employee(id):
+        employee = Employee.query.get(id)
+        if not employee:
+            return "Employee not found", 404
+        return render_template("view_employee.html.jinja", employee=employee)
+
+    @app.route("/employees/<int:id>/delete", methods=["GET","POST"])
+    def delete_employee(id):
+        employee = Employee.query.get(id)
+        if not employee:
+            return "Employee not found", 404
+        if request.method == "POST":
+            try:
+                db.session.delete(employee)
+                db.session.commit()
+                flash("Employee {} record deleted successfully!".format(employee.full_name))
+                return redirect("/employees")
+            except:
+                flash("Employee record not deleted!")
+                return redirect("/employees/{}".format(employee.id))
+        else:
+            assert request.method == "GET"
+            return render_template("delete_employee.html.jinja", employee=employee)
 
     return app 
